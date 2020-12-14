24087027220a156d49e1ea10feddd004947d4bb7
Ezequiel Bitencourt <ezequiel.bitencourt197@gmail.com>
2020-12-14 13:46:42 -0300

    Feature Delete function and icons update in LeaveTypeController

--- leave-management/Controllers/LeaveTypesController.cs
+++ leave-management/Controllers/LeaveTypesController.cs
@@ -121,21 +121,41 @@ namespace leave_management.Controllers
         // GET: LeaveTypesController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            var leavetype = _repo.FindById(id);
+            if (leavetype == null)
+            {
+                return NotFound();
+            }
+            var isSuccess = _repo.Delete(leavetype);
+            if (!isSuccess)
+            {
+                return BadRequest();
+            }
+            return RedirectToAction(nameof(Index));
         }
 
         // POST: LeaveTypesController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, LeaveTypeVM model)
         {
             try
             {
+                var leavetype = _repo.FindById(id);
+                if(leavetype== null)
+                {
+                    return NotFound();
+                } 
+                var isSuccess = _repo.Delete(leavetype);
+                if (!isSuccess)
+                {
+                    return View(model);
+                }
                 return RedirectToAction(nameof(Index));
             }
             catch
             {
-                return View();
+                return View(model);
             }
         }
     }

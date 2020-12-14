d368a534e41f6a19e14a27d3ad0cc95b6b0f69e5
Ezequiel Bitencourt <ezequiel.bitencourt197@gmail.com>
2020-12-14 10:55:44 -0300

    Add project files.

---
+++ leave-management/Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,11 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System.Collections.Generic;
+
+namespace leave_management.Repository
+{
+    public interface ILeaveTypeRepository : IRepositoryBase<LeaveType>
+    {
+        ICollection<LeaveType> GetEmployeesByLeaveType(int id);
+    }
+}
---
+++ leave-management/Models/LeaveAllocationVM.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace leave_management.Models
+{
+    public class LeaveAllocationVM
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int NumberOfDays { get; set; }
+
+        public DateTime DateCreated { get; set; }
+
+        public EmployeeVM Employee { get; set; }
+
+        public string EmployeeId { get; set; }
+
+        public DetailsLeaveTypeVM LeaveType { get; set; }
+
+        public int LeaveTypeId { get; set; }
+
+        public IEquatable<SelectListItem> Employees { get; set; }
+        public IEquatable<SelectListItem> LeaveTypes { get; set; }
+    }
+}
